be1bf86f48173013a5ac34d43ab5b3e7b7700476
Dylan Lutton <dylandlutton@gmail.com>
2019-05-27 15:30:37 +0200

    Adding POST requests

--- headlines.py
+++ headlines.py
@@ -23,9 +23,9 @@ RSS_FEEDS = {'anxiety':'https://www.psychiatryadvisor.com/home/topics/anxiety/fe
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def get_news():
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "anxiety"
     else:
@@ -41,8 +41,8 @@ if __name__ == '__main__':
   
   
   
-  
-  
+
+
   
   
   
