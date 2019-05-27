8f5cd3db53d0ed242c979d74489c059edf8d3cbc
Dylan Lutton <dylandlutton@gmail.com>
2019-05-27 10:47:28 +0200

    Adding Jinja templating

--- headlines.py
+++ headlines.py
@@ -8,7 +8,7 @@ Created on Wed May 22 20:28:56 2019
 
 import feedparser
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 RSS_FEEDS = {'anxiety':'https://www.psychiatryadvisor.com/home/topics/anxiety/feed',
@@ -16,8 +16,7 @@ RSS_FEEDS = {'anxiety':'https://www.psychiatryadvisor.com/home/topics/anxiety/fe
                  'depression':'https://www.nature.com/subjects/depression.rss',
                  'dwarves':'http://www.bay12games.com/dwarves/dev_now.rss',
                  'obitOne':'https://www.nytimes.com/services/xml/rss/nyt/Obituaries.xml',
-                 'obitTwo':'https://www.bucktroutfuneralhome.net/obituary/feed/',
-                 'leprosy':'http://www.wpro.who.int/feeds/entity/laos/en/rss.xml',
+                 'emergencies':'https://afro.who.int/rss/emergencies.xml',
                  'death':'http://rawdataserver.com/CDB/rss'
                  }
 
@@ -28,16 +27,7 @@ RSS_FEEDS = {'anxiety':'https://www.psychiatryadvisor.com/home/topics/anxiety/fe
 def get_news(publication="anxiety"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-    <body>
-        <h1>Headlines </h1>
-        <b>{0}</b> <br/>
-        <i>{1}</i> <br/>
-        <p>{2}</p> <br/>
-    </body>
-</html>""".format(first_article.get("title"),
- first_article.get("published"),
- first_article.get("summary"))
+    return render_template("home.html")
 
 if __name__ == '__main__':
   app.run(port=5000, debug=True)
